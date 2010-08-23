b530931f806c753f5011b27c78751f169334f2c5
Whiteknight <wknight8111@gmail.com>
2010-08-22 20:48:52 -0400

    a few small cleanups

--- src/include/pla_matrix_library.h
+++ src/include/pla_matrix_library.h
@@ -2,9 +2,11 @@
 #define _PLA_MATRIX_LIBRARY_H_
 
 INTVAL floats_are_equal(FLOATVAL a, FLOATVAL b);
-void get_complex_value_from_pmc(PARROT_INTERP, PMC * value, FLOATVAL * real, FLOATVAL * imag);
+void get_complex_value_from_pmc(PARROT_INTERP, PMC * value, FLOATVAL * real,
+    FLOATVAL * imag);
 
-void intkey_to_coords(PARROT_INTERP, const INTVAL rows, const INTVAL cols, const INTVAL key, INTVAL * row, INTVAL * col);
+void intkey_to_coords(PARROT_INTERP, const INTVAL rows, const INTVAL cols,
+    const INTVAL key, INTVAL * row, INTVAL * col);
 
 
 #endif /* _PLA_MATRIX_LIBRARY_H */
