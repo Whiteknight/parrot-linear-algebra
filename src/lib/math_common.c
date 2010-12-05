01e58f575284ed744096891b79eff8f416bfd7df
Whiteknight <wknight8111@gmail.com>
2010-12-04 21:40:24 -0500

    small tweaks and cleanups

--- src/lib/math_common.c
+++ src/lib/math_common.c
@@ -45,15 +45,14 @@ get_complex_value_from_pmc(PARROT_INTERP, PMC * value, FLOATVAL * real,
         *imag = VTABLE_get_number_keyed_int(interp, value, 1);
     }
     else if (VTABLE_does(interp, value, Parrot_str_new(interp, "array", 5))) {
-        INTVAL size = VTABLE_elements(interp, value);
+        const INTVAL size = VTABLE_elements(interp, value);
         if(size <= 2) {
             *real = (size == 0) ? 0.0 : VTABLE_get_number_keyed_int(interp, value, 0);
             *imag = (size < 2) ? 0.0 : VTABLE_get_number_keyed_int(interp, value, 1);
         }
-		else {
-			Parrot_ex_throw_from_c_args(interp, NULL, EXCEPTION_OUT_OF_BOUNDS,
-            "PLA: array too long to be converted into complex number");
-		}
+        else
+            Parrot_ex_throw_from_c_args(interp, NULL, EXCEPTION_OUT_OF_BOUNDS,
+               "PLA: array too long to be converted into complex number");
     }
     else if (VTABLE_does(interp, value, Parrot_str_new(interp, "float", 5))) {
         *real = VTABLE_get_number(interp, value);
@@ -69,8 +68,7 @@ get_complex_value_from_pmc(PARROT_INTERP, PMC * value, FLOATVAL * real,
         *real = VTABLE_get_number_keyed_int(interp, c, 0);
         *imag = VTABLE_get_number_keyed_int(interp, c, 1);
     }
-    else {
+    else
         Parrot_ex_throw_from_c_args(interp, NULL, EXCEPTION_OUT_OF_BOUNDS,
             "PLA: cannot set unknown PMC type");
-    }
 }
